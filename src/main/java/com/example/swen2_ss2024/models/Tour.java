d9154d603f1cf9683c3083bfcd0bfc4f166f020b
Patricia <tpatricia.stefania@gmail.com>
2024-05-10 18:19:34 +0200

    Tour class created

--- src/main/java/com/example/swen2_ss2024/models/Tour.java
+++ src/main/java/com/example/swen2_ss2024/models/Tour.java
@@ -1,47 +1,99 @@
 package com.example.swen2_ss2024.models;
 
-public class Tour {
-    private String name;
-    private String description;
-    /*private String from;
-    private String to;
-    private String transportType;
-    private double distance;  // in kilometers
-    private double estimatedTime; // in hours
-    private String routeInformation; // URL to the image map*/
+import java.util.Arrays;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-    public Tour(String name, String description /*,String from, String to, String transportType, double distance, double estimatedTime, String routeInformation*/) {
+public class Tour {
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("description")
+    private String description;
+
+    @JsonProperty("from")
+    private String from;
+
+    @JsonProperty("to")
+    private String to;
+
+    @JsonProperty("transport_type")
+    private String transportType;
+
+    @JsonProperty("distance")
+    private String distance;
+
+    @JsonProperty("estimated_time")
+    private String estimatedTime;
+
+    public Tour(String name, String description, String from, String to, String transportType, String distance, String estimatedTime) {
         this.name = name;
         this.description = description;
-        /*this.from = from;
+        this.from = from;
         this.to = to;
         this.transportType = transportType;
         this.distance = distance;
         this.estimatedTime = estimatedTime;
-        this.routeInformation = routeInformation;*/
     }
 
-    // Getters
-    public String getName() { return name; }
-    public String getDescription() { return description; }
-    /*public String getFrom() { return from; }
-    public String getTo() { return to; }
-    public String getTransportType() { return transportType; }
-    public double getDistance() { return distance; }
-    public double getEstimatedTime() { return estimatedTime; }
-    public String getRouteInformation() { return routeInformation; }*/
+    public String getName() {
+        return name;
+    }
 
-    // Setters
-    public void setName(String name) { this.name = name; }
-    public void setDescription(String description) { this.description = description; }
-    /*public void setFrom(String from) { this.from = from; }
-    public void setTo(String to) { this.to = to; }
-    public void setTransportType(String transportType) { this.transportType = transportType; }
-    public void setDistance(double distance) { this.distance = distance; }
-    public void setEstimatedTime(double estimatedTime) { this.estimatedTime = estimatedTime; }
-    public void setRouteInformation(String routeInformation) { this.routeInformation = routeInformation; }*/
-    @Override
-    public String toString() {
-        return name + " - " + description; // Or any other format you prefer
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getTransportType() {
+        return transportType;
+    }
+
+    public void setTransportType(String transportType) {
+        this.transportType = transportType;
+    }
+
+    public String getDistance() {
+        return distance;
+    }
+
+    public void setDistance(String distance) {
+        this.distance = distance;
+    }
+
+    public String getEstimatedTime() {
+        return estimatedTime;
+    }
+
+    public void setEstimatedTime(String estimatedTime) {
+        this.estimatedTime = estimatedTime;
+    }
+
+    public List<Object> getFieldsAsList() {
+        return Arrays.asList(name, description, from, to, transportType, distance, estimatedTime);
     }
 }
+
